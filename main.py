b71cf9fa8158e5ca0bfd0717eadb7da0700311ec
Olllibaba <95749661+Olllibaba@users.noreply.github.com>
2021-12-13 13:37:07 +0100

    a

---
+++ main.py
@@ -0,0 +1,25 @@
+# This is a sample Python script.
+import pandas as pd
+import requests
+# input ticker
+
+# function to get expiration dates
+
+# function to get options data for Ticker + Expiration Date
+
+import requests
+
+
+#This one works
+#page = requests.get("https://www.dataquest.io/blog/web-scraping-python-using-beautiful-soup/")
+
+url="https://finance.yahoo.com/quote/TSLA/options?p=TSLA&date=1705622400"
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:75.0) Gecko/20100101 Firefox/75.0'}
+resp = requests.get(url, headers=headers, timeout=5).text
+print(resp)
+
+#data = pd.read_html(link)
+
+# function to find Minimum for Ticker + Expiration Date
+
+# project Trajectory
